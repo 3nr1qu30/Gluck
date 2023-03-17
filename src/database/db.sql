adc697bf0603c99f05d9aaa2f0c90c35ffff2e5f
3nr1qu30 <evarillasf2000@alumno.ipn.mx>
2023-03-16 21:55:44 -0600

    Login sin encriptar

--- src/database/db.sql
+++ src/database/db.sql
@@ -4,7 +4,7 @@ use Glucky;
 create table pacientes(
 Curp varchar(18) primary key not null,
 Email varchar(70),
-Pass varchar(70),
+Pass varchar(100),
 Nombre varchar(50),
 Apellidos varchar(60),
 Sexo varchar(10),
@@ -14,16 +14,16 @@ Telefono varchar(10));
 create table doctores(
 Cedula varchar(8) primary key not null,
 Email varchar(70),
-Pass varchar(70),
+Pass varchar(100),
 Nombre varchar(50),
 Apellidos varchar(60),
 Sexo varchar(10),
 Calle varchar (40),
+NumeroCa varchar(5),
 Colonia varchar(40),
 Del_o_Muni varchar(40),
 Codigo_Postal int,
-Estado varchar(40),
-Ciudad varchar(40),
+EstadoEntidadFe varchar(40),
 Telefono varchar(10),
 Edad int);
 create table datosmedicos(
